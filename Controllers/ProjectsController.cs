18fb582c7a460108929b4f7bb17f86ea9bb67f1a
Domoina Ramahefanimanana <dar@esokia-webagency.com>
2024-02-12 15:41:45 +0400

    feat(code review) : rename variables

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -6,29 +6,34 @@ using ProjectTestDotNet.Repository;
 namespace ProjectTestDotNet.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
     public class ProjectsController : ControllerBase
     {
+        private readonly IMapper _mapper;
+
+        public ProjectsController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
         [HttpGet]
-        [Route("api/getProject")]
+        [Route("api/project/all")]
         public ObjectResult GetProjects()
         {
             try
             {
-                // Extracted mapping configuration
-                var mapper = CreateMapper();
+                
 
-                var dtos = new List<ProjectDTO>();
-                using (var context = new Projectrepository())
+                var allProjectDtos = new List<ProjectDTO>();
+                using (var repos = new Projectrepository())
                 {
                     // Retrieve all project
-                    var allEmployees = context.project.ToList();
-                    foreach (var employee in allEmployees)
+                    var allProjects = repos.project.ToList();
+                    foreach (var project in allProjects)
                     {
-                        dtos.Add(mapper.Map<ProjectDTO>(employee));
+                        allProjectDtos.Add(_mapper.Map<ProjectDTO>(project));
 
                     }
-                    return StatusCode(200, dtos);
+                    return StatusCode(200, allProjectDtos);
                 }
             }
             catch(Exception e)
@@ -39,20 +44,18 @@ namespace ProjectTestDotNet.Controllers
 
 
         [HttpPost]
-        [Route("api/saveProject")]
+        [Route("api/project/save")]
         public ObjectResult SaveProject(ProjectDTO model)
         {
             try
             {
-                // Extracted mapping configuration
-                var mapper  = CreateMapper();
-                var project = mapper.Map<Project>(model);
-                using (var context = new Projectrepository())
+                var project = _mapper.Map<Project>(model);
+                using (var repo = new Projectrepository())
                 {
-                    // Retrieve all employees
-                    var allEmployees = context.project.Add(project);
+                    // Retrieve all project
+                    repo.project.Add(project);
 
-                    context.SaveChanges();
+                    repo.SaveChanges();
                     return StatusCode(200, model);
 
                 }
@@ -61,28 +64,6 @@ namespace ProjectTestDotNet.Controllers
             {
                 return StatusCode(500, "An error occurred while processing the request.");
             }
-        }
-
-        private IMapper CreateMapper()
-        {
-            var config = new MapperConfiguration(cfg => {
-                // DTO to Domain Model
-                cfg.CreateMap<Project, ProjectDTO>().ForMember(dest => dest.weather, opt => opt.MapFrom(src => src.temp2))
-                    .ForMember(dest => dest.date, opt => opt.MapFrom(src => src._date))
-                    .ForMember(dest => dest.workingHours, opt => opt.MapFrom(src => src.horaires))
-                    .ForMember(dest => dest.workAt, opt => opt.MapFrom(src => src.travail))
-                    .ForMember(dest => dest.temperatureMorning, opt => opt.MapFrom(src => src.meteo))
-                    .ForMember(dest => dest.temperatureAfternoon, opt => opt.MapFrom(src => src.temp1));
-
-                // Domain Model to DTO
-                cfg.CreateMap<ProjectDTO, Project>().ForMember(dest => dest._date, opt => opt.MapFrom(src => src.date))
-                    .ForMember(dest => dest.horaires, opt => opt.MapFrom(src => src.workingHours))
-                    .ForMember(dest => dest.travail, opt => opt.MapFrom(src => src.workAt))
-                    .ForMember(dest => dest.meteo, opt => opt.MapFrom(src => src.temperatureMorning))
-                    .ForMember(dest => dest.temp1, opt => opt.MapFrom(src => src.temperatureAfternoon)); 
-            });
-
-            return new Mapper(config);
         }
 
     }
